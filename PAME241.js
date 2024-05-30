0a77cb5b664fa0e892454af8df4096e54fa7e9d6
Marcos-Deodato <marcosmacieldeodato.20232@poli.ufrj.br>
2024-05-30 00:04:26 -0300

    main

---
+++ PAME241.js
@@ -0,0 +1,41 @@
+class Pedido {
+    constructor(id, clienteId, status, data) {
+      this.id = id;
+      this.clienteId = clienteId;
+      this.status = status;
+      this.data = data;
+    }
+ 
+
+class Funcionario {
+    constructor(id, nomeUsuario, cpf, email, senha) {
+       this.id = id;
+       this.nomeUsuario = nomeUsuario;
+       this.cpf = cpf;
+       this.email = email;
+       this.senha = senha;
+    }
+      
+class Cliente {
+    constructor(id, nome, dataNascimento, cpf, email, senha) {
+       this.id = id;
+       this.nome = nome;
+       this.dataNascimento = dataNascimento;
+       this.cpf = cpf;
+       this.email = email;
+       this.senha = senha;
+    }
+class Produto {
+    constructor(id, nome, descricao, validade, preco, quantidade) {
+       this.id = id;
+       this.nome = nome;
+       this.descricao = descricao;
+       this.validade = validade;
+       this.preco = preco;
+       this.quantidade = quantidade;
+    }
+      
+       
+          
+              
+  
